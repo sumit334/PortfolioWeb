bed3886ee224962ffa958d9f22746c96946d3fc7
Sumit Agarwal <97662774+sumit334@users.noreply.github.com>
2023-09-14 18:50:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Devloper", "Coder", "Blogger" , "Learner"],
+        strings: ["Web Developer", "Coder", "Blogger" , "Learner"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Devloper", "Coder", "Blogger" , "Learner"],
+        strings: ["Web Developer", "Coder", "Blogger" , "Learner"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
